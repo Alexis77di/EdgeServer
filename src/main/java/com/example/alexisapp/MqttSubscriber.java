9563231898d24de0d1cde2d85bcf3ad1b2c3e417
Alexey77 <sdi1300197@di.uoa.gr>
2018-12-05 04:20:08 +0200

    Usefull Mqtt changes!

---
+++ src/main/java/com/example/alexisapp/MqttSubscriber.java
@@ -0,0 +1,84 @@
+package com.example.alexisapp;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MqttSubscriber implements MqttCallback {
+
+    private static MqttPublisher publisher;
+
+    //String macAddress;
+    public static void main(String[] args) {
+
+        String topic = "#";
+        int qos = 2;
+        String broker = "tcp://localhost:1883";
+        String clientId = "Μyclientid2";
+        MemoryPersistence persistence = new MemoryPersistence();
+        try {
+            MqttAsyncClient sampleClient = new MqttAsyncClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            sampleClient.setCallback(new MqttSubscriber());
+            System.out.println("Connecting to broker: " + broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            Thread.sleep(1000);
+            sampleClient.subscribe(topic, qos);
+            System.out.println("Subscribed");
+        } catch (Exception me) {
+            if (me instanceof MqttException) {
+                System.out.println("reason " + ((MqttException) me).getReasonCode());
+            }
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+
+    }
+
+    public void connectionLost(Throwable cause) {
+        System.out.println("Connection lost!" + cause);
+        System.exit(1);
+    }
+
+    public void deliveryComplete(IMqttDeliveryToken token) {
+
+    }
+
+    public void messageArrived(String topic, MqttMessage message) {
+        System.out.println("topic: " + topic);
+        System.out.println("message: " + new String(message.getPayload()));
+//        ManagerThread mng_thread = new ManagerThread(topic);
+//        Thread tmp_thread = new Thread(mng_thread);
+//        tmp_thread.start();
+    }
+
+    private class ManagerThread implements Runnable {
+
+        String[] arr;
+
+        public ManagerThread(String topic) {
+
+            arr = topic.split("/");
+
+
+        }
+
+        @Override
+        public void run() {
+//            int cntr = arr.length;
+//            if(cntr == 4){
+//                String macAddress = arr[0];
+//                String sensor_Value = arr[1];
+//                String latitude = arr[2];
+//                String longtitude = arr[3];
+//            }
+            publisher = new MqttPublisher();
+            String macAddress = "alexey shved";
+            publisher.main(macAddress, "Hello from the other side!");
+        }
+    }
+}
